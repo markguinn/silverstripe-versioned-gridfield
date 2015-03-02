088fcaea11a44275f2e152320cc7ee27a04df4ce
Mark Guinn <mark@adaircreative.com>
2015-03-02 13:05:10 +0000

    disabled this module

--- code/VersionedModelAdmin.php
+++ code/VersionedModelAdmin.php
@@ -8,25 +8,25 @@
  */
 class VersionedModelAdmin extends Extension {
 
-	function onBeforeInit() {
-		Versioned::reading_stage('Stage');
-	}
-
-	function updateEditForm($form) {
-		$fieldList = $form->Fields();
-
-		foreach($fieldList as $field) {
-			if($field instanceof GridField) {
-				$class = $field->getList()->dataClass();
-				if($class::has_extension("Versioned")) {
-					$config = $field->getConfig();
-					$config->removeComponentsByType('GridFieldDeleteAction')
-						->removeComponentsByType('GridFieldDetailForm')
-						->addComponents(new VersionedGridFieldDetailForm());
-					$field->setConfig($config);
-				}
-			}
-		}
-	}
+//	function onBeforeInit() {
+//		Versioned::reading_stage('Stage');
+//	}
+//
+//	function updateEditForm($form) {
+//		$fieldList = $form->Fields();
+//
+//		foreach($fieldList as $field) {
+//			if($field instanceof GridField) {
+//				$class = $field->getList()->dataClass();
+//				if($class::has_extension("Versioned")) {
+//					$config = $field->getConfig();
+//					$config->removeComponentsByType('GridFieldDeleteAction')
+//						->removeComponentsByType('GridFieldDetailForm')
+//						->addComponents(new VersionedGridFieldDetailForm());
+//					$field->setConfig($config);
+//				}
+//			}
+//		}
+//	}
 
 }
